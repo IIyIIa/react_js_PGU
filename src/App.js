452c7b6b7bc81c7fb2fb6f57e627b987ff0498bb
Nikita <pupanov67@mail.ru>
2023-04-27 10:39:08 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  console.log('Test OK!!!!!!!')
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
+      fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        setPosts(data);
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+      });
+
+  }, []);
+
+  const getUserPost = (userId) => {
+    return posts.find(post => post.userId === userId);
+  };
+
+
+  return (
+    <div className="App">
+      <h1>Pupanov 0432-06 React</h1>
+      {users.map(user => {
+        const userPost = getUserPost(user.id);
+
+        return (
+          <div key={user.id} className="user-card">
+            <h2>{user.name} ({user.username})</h2>
+            <p>mail: {user.email}</p>
+            <p>phone number: {user.phone}</p>
+            <p>company name: "{user.company.name}"</p>
+            {userPost && (
+              <div className="user-post">
+                <h3>User Post:</h3>
+                <p className="post-title larger-text">{userPost.title}</p>
+                <p>{userPost.body}</p>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
